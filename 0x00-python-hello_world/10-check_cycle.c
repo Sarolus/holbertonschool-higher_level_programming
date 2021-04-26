ce2f411a9fdad9ecb333f4e81232ab3f8fe663b3
Sarolus <cactussius.primus@gmail.com>
2021-04-26 16:47:49 +0200

    Fix indentation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - Checks if a singly linked list has a cycle in it.
- * @list: the checked list
+ * @list: the head of the list
  * Return: return 0 if there is no cycle, otherwise 1 if there is.
  */
 int check_cycle(listint_t *list)
@@ -12,8 +12,8 @@ int check_cycle(listint_t *list)
 
 	while (!tmp && !head->next && !head->next->next)
 	{
-		head = head->next->next;
-		tmp = tmp->next;
+		head = head->next;
+		tmp = tmp->next->next;
 
 		if (head == tmp)
 		{
