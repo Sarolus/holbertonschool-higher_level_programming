a94a18f37ea26c8af33a5cafd03bb5e9bb7e7f57
Sarolus <cactussius.primus@gmail.com>
2021-04-29 13:52:23 +0200

    Add if main condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = len(sys.argv)
-if (args == 1):
-    print("{} arguments.".format(args - 1))
-elif (args == 2):
-    print("{} argument:".format(args - 1))
-else:
-    print("{} arguments:".format(args - 1))
+    args = len(sys.argv)
+    if (args == 1):
+        print("{} arguments.".format(args - 1))
+    elif (args == 2):
+        print("{} argument:".format(args - 1))
+    else:
+        print("{} arguments:".format(args - 1))
 
-for i in range(1, args):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, args):
+        print("{}: {}".format(i, sys.argv[i]))
