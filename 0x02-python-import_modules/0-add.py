aa005fb86df9d9c37e3c8f2d5809b7ed60556795
Sarolus <cactussius.primus@gmail.com>
2021-04-29 13:03:47 +0200

    Fix script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-def main():
-    from add_0 import add
+#!/usr/bin/python3
+from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    main()
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
