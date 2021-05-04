7d9fadbe082ce6b64585a0a7909742bd8a8a4b75
Sarolus <cactussius.primus@gmail.com>
2021-05-04 23:15:17 +0200

    Fix freaking pebkac MAJ

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -21,19 +21,17 @@ void print_item_info(PyObject *item, int itemIndex)
  */
 void print_python_list_info(PyObject *p)
 {
-	int itemIndex, objAllocatedNb = 0;
-	PyObject *item;
+	int itemIndex, allocObjNb = 0;
 	Py_ssize_t objListSize = 0;
+	PyObject *item;
 
-	/* Check if item list is not empty */
 	if (PyList_Check(p))
 	{
 		objListSize = PyList_Size(p);
-		objAllocatedNb = ((PyListObject *)p)->allocated;
+		allocObjNb = ((PyListObject *)p)->allocated;
 
 		printf("[*] Size of the Python List = %d\n", (int)objListSize);
-		printf("[*] Allocated = %d\n", objAllocatedNb);
-
+		printf("[*] Allocated = %d\n", allocObjNb);
 		for (itemIndex = 0; itemIndex < objListSize; itemIndex++)
 		{
 			item = PyList_GetItem(p, itemIndex);
