13cad0322f1d28a27c8aaa779d86528eff5b07f6
Sarolus <cactussius.primus@gmail.com>
2021-05-04 16:22:59 +0200

    Fix condition for spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in range(len(matrix)):
         for column in range(len(matrix[line])):
-            print(matrix[line][column], end=" ")
+            print("{:d}".format(matrix[line][column]), end='')
+            if column < (len(matrix[line]) - 1):
+                print(" ", end='')
         print()
