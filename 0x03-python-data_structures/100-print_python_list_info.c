4957ff0436fd7ff3779032acd2bd32e9aeb7d512
Sarolus <cactussius.primus@gmail.com>
2021-05-04 22:55:49 +0200

    Fix itemName parameter

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,14 +3,14 @@
 
 /**
  * print_item_info - Print the information of items of a python list
- * @Obj: Item of an python object.
+ * @Item: Item of an python object.
  * @itemIndex: Index of the items of a python list.
  */
-void print_item_info(PyObject *Obj, int itemIndex)
+void print_item_info(PyObject *Item, int itemIndex)
 {
 	char *itemName;
 
-	itemName = Py_TYPE(Obj)->tp_name;
+	itemName = (char *)Py_TYPE(Item)->tp_name;
 	printf("Element %d: %s\n", itemIndex, itemName);
 }
 
@@ -23,19 +23,19 @@ void print_python_list_info(PyObject *p)
 {
 	int itemIndex, allocObjNb;
 	Py_ssize_t objListsize = 0;
-	PyObject *Obj;
+	PyObject *Item;
 
 	if (PyList_Check(p))
 	{
-		objListsize = PyList_SIZE(p);
+		objListsize = PyList_Size(p);
 		allocObjNb = ((PyListObject *)p)->allocated;
 
-		printf("[*] Size of the Python list = %d\n", objListsize);
+		printf("[*] Size of the Python list = %d\n", (int)objListsize);
 		printf("[*] Allocated = %d\n", allocObjNb);
 		for (itemIndex = 0; itemIndex < objListsize; itemIndex++)
 		{
-			Obj = PyList_GetItem(p, itemIndex);
-			print_item_info(Obj, itemIndex);
+			Item = PyList_GetItem(p, itemIndex);
+			print_item_info(Item, itemIndex);
 		}
 	}
 }
