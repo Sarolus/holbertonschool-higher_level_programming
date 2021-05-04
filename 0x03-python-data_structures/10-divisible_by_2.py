74e09b5e043d4125ffbebc7f27816962fcc5265c
Sarolus <cactussius.primus@gmail.com>
2021-05-04 17:31:56 +0200

    Add task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    bool = []
+    for n in range(len(my_list)):
+        if 0 == n % 2:
+            bool.append(True)
+        else:
+            bool.append(False)
+    
+    return bool
