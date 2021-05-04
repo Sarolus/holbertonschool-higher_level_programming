dcb761e898e75b76078e7cc77fd73f2cae45c8fb
Sarolus <cactussius.primus@gmail.com>
2021-05-04 18:02:38 +0200

    Get back to functional

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
-        return my_list
-    elif idx in my_list:
+    if idx < len(my_list) and idx >= 0:
         my_list.remove(my_list[idx])
         return my_list
