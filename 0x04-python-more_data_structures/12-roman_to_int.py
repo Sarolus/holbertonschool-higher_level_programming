2a5d54d2caaca427c4a8924a746d0da6e396abd0
Sarolus <cactussius.primus@gmail.com>
2021-05-06 22:10:02 +0200

    Fix Substract value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,5 +2,10 @@
 def roman_to_int(roman_string):
     if (roman_string is None or type(roman_string) is not str):
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return sum(roman[nb] for nb in roman_string)
+    Value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    SubValue = {"IV": 2, "IX": 2, "XL": 20, "XC": 20, "CD": 200, "CM": 200}
+
+    int = sum(Value[char] for char in roman_string)
+    int -= sum(value for key, value in SubValue.items() if key in roman_string)
+
+    return int
