a1f448d25a1765017bbd545da2aa2145eb48f983
Sarolus <cactussius.primus@gmail.com>
2021-05-18 16:19:01 +0200

    Fix Google Style Python Docstrings

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+
+"""
+Square class
+
+This class defines an square, initialize it's size and
+give it's area.
+
+"""
+
+
 class Square():
+    """Square Class"""
     def __init__(self, size=0):
+        """
+        __init__ method
+        Args:
+            size (int): size of a square
+        Raises:
+            TypeError: Error if size is not an int.
+            ValueError: Error if size is less than 0.
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -8,4 +27,9 @@ class Square():
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """
+        area method
+
+        Returns the area of a square
+        """
         return self.__size * self.__size
