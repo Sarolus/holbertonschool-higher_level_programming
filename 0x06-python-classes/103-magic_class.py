ae8cf1f3a13a80eccd417dee53c8039d7e72b76f
Sarolus <cactussius.primus@gmail.com>
2021-05-19 23:02:17 +0200

    Fix missing comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
+"""
+Magic class Task 10
+
+This class was retrieved from Bytecodes for the task 10 of project
+0x06-python-classes.
+"""
+
 import math
 
 
 class MagicCLass:
+    """
+    This class defines a circle of radius self.__radius
+    """
+
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
