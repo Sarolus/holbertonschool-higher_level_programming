89e90affbb5e076437ddc15dc7cd83e7651f9aa0
Sarolus <cactussius.primus@gmail.com>
2021-06-01 18:33:34 +0200

    Add doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+    Module containing lookup function
+"""
+
+
 def lookup(obj):
     """
         Return the list of a available attributes and
         methods of an object
     """
-    return (dir(obj))
+    return dir(obj)
