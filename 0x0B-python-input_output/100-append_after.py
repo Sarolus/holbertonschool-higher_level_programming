a92e42a29502bf9208d27d2a439c7d198dd294db
Sarolus <cactussius.primus@gmail.com>
2021-06-02 16:52:41 +0200

    Add task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    Search and Update Module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+        Insert a new line to a file, after each line
+        containing a specific string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            new_line += line
+            if search_string in line:
+                new_line += new_string
+
+    with open(filename, mode="w") as file:
+        file.write(new_line)
