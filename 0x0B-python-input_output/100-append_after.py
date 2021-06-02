a3570a3fa22039f6db9bd49c0d7951991cac64bd
Sarolus <cactussius.primus@gmail.com>
2021-06-02 16:55:40 +0200

    Fix local variable

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,6 +10,7 @@ def append_after(filename="", search_string="", new_string=""):
         Insert a new line to a file, after each line
         containing a specific string.
     """
+    new_line = ""
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             new_line += line
