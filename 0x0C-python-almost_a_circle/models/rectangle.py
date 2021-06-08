b5bbce850bd148850b1cb05ea7dfc16bcff0b888
Sarolus <cactussius.primus@gmail.com>
2021-06-08 17:19:19 +0200

    Manage Index Error case

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,11 +126,14 @@ class Rectangle(Base):
             Assign an argument to each attribute
         """
         if args:
-            self.id = args[0]
-            self.__width = args[1]
-            self.__height = args[2]
-            self.__x = args[3]
-            self.__y = args[4]
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.__height = args[2]
+                self.__x = args[3]
+                self.__y = args[4]
+            except IndexError:
+                pass
         else:
             for key, value in kwargs.items():
                 self.__setattr__(key, value)
