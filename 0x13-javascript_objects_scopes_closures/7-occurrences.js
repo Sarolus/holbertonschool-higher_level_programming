2867d2a1516384fafec078b145f03af74e9ede5e
Sarolus <cactussius.primus@gmail.com>
2021-08-03 13:53:31 +0200

    Fix task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurence = function (list, searchElement) {
-  let i = 0;
-  list.forEach(x => {
-    if (x === searchElement) {
-      i++;
+  let occurence = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occurence += 1;
     }
-  });
-  return i;
+  }
+  return (occurence);
 };
