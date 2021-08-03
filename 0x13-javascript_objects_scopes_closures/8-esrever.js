d7e2ca72b01f72dbdc27f78293524aa2e124a1fa
Sarolus <cactussius.primus@gmail.com>
2021-08-03 14:04:57 +0200

    Semistandard fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const newList = [];
+  const newList = [];
 
-    list.forEach(element => {
-        newList.unshift(element);
-    });
-    return (newList);
+  list.forEach(element => {
+    newList.unshift(element);
+  });
+  return (newList);
 };
