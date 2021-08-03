cfd1adb8305c9f5bcaccb54d2bf92c7bb89687d6
Sarolus <cactussius.primus@gmail.com>
2021-08-03 14:28:20 +0200

    FIX

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+let index = 0;
 const list = require('./100-data.js').list;
 
 console.log(list);
-const map1 = list.map((value, index) => value * index);
+const map1 = list.map(x => x * index++);
 console.log(map1);
