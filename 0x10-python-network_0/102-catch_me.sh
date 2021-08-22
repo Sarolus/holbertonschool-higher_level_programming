98ef00fab706dfffbc014959ecb52308672e54cf
Sarolus <cactussius.primus@gmail.com>
2021-08-22 13:56:40 +0200

    Add task 8 & 9

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX PUT -d "user_id=98" -H "Origin: HolbertonSchool" "localhost:5000/catch_me"
