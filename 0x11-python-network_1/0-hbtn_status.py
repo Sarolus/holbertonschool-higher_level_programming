590dc82a25ee1f00bd9090aa5424f58187d5931f
Sarolus <cactussius.primus@gmail.com>
2021-08-22 14:42:19 +0200

    Add task 0 & README.md

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Script that fetches https://intranet.hbtn.io/status
+"""
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = request.Request(url)
+    with request.urlopen(req) as response:
+        content = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
