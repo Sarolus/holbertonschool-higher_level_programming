41e45a198425c55a61ddac236a3b1cebc9ce9dba
Sarolus <cactussius.primus@gmail.com>
2021-09-22 17:17:21 +0200

    Add tasks 6 to 9

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,3 +1,3 @@
-$('div#add_item').on('click', function () {
-    $('ul.my_list').append('<li>Item</li>');
-})
+$('DIV#add_item').on('click', function () {
+    $('UL.my_list').append('<li>Item</li>');
+});
