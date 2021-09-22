988f657fc2b529ef8e63d7fc1f720c81789e061d
Sarolus <cactussius.primus@gmail.com>
2021-09-22 18:32:59 +0200

    Semistandard compliant

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,8 @@
-window.onload = function () {
-    $('INPUT#btn_translate').on('click', function () {
-        const languageCode = $('INPUT#language_code').val();
-        $.get('https://fourtonfish.com/hellosalut/?lang=' + languageCode, function (data) {
-            $('DIV#hello').text(data.hello);
-        });
+$(document).ready(function () {
+  $('INPUT#btn_translate').on('click', function () {
+    const languageCode = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + languageCode, function (data) {
+      $('DIV#hello').text(data.hello);
     });
-};
+  });
+});
